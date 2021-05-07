8ec5d8c224e1c8e00a00a17fe451eb306f181e32
Claudio Filho <claudioasfilho@gmail.com>
2021-05-07 16:13:25 -0700

    first commit

---
+++ GNU ARM v7.2.1 - Default/gecko_sdk_3.1.1/app/bluetooth/common/ncp_evt_filter/sl_ncp_evt_filter.d
@@ -0,0 +1,26 @@
+gecko_sdk_3.1.1/app/bluetooth/common/ncp_evt_filter/sl_ncp_evt_filter.o: \
+ /Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1/app/bluetooth/common/ncp_evt_filter/sl_ncp_evt_filter.c \
+ /Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1//platform/common/toolchain/inc/sl_gcc_preinclude.h \
+ /Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1//platform/common/inc/sl_status.h \
+ /Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1/app/bluetooth/common/ncp_evt_filter/sl_ncp_evt_filter.h \
+ /Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1//protocol/bluetooth/inc/sl_bt_api.h \
+ /Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1//protocol/bluetooth/inc/sl_bgapi.h \
+ /Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1//protocol/bluetooth/inc/sl_bt_api_compatibility.h \
+ /Users/clfilho/SimplicityStudio/May_21/ncp_32CN/config/sl_ncp_evt_filter_config.h \
+ /Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1/app/bluetooth/common/ncp_evt_filter/sl_ncp_evt_filter_common.h
+
+/Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1//platform/common/toolchain/inc/sl_gcc_preinclude.h:
+
+/Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1//platform/common/inc/sl_status.h:
+
+/Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1/app/bluetooth/common/ncp_evt_filter/sl_ncp_evt_filter.h:
+
+/Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1//protocol/bluetooth/inc/sl_bt_api.h:
+
+/Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1//protocol/bluetooth/inc/sl_bgapi.h:
+
+/Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1//protocol/bluetooth/inc/sl_bt_api_compatibility.h:
+
+/Users/clfilho/SimplicityStudio/May_21/ncp_32CN/config/sl_ncp_evt_filter_config.h:
+
+/Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1/app/bluetooth/common/ncp_evt_filter/sl_ncp_evt_filter_common.h:
